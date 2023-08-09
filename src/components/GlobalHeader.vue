d05749b72707e2edd227198eb6da1c8713143397
zyz19 <80141182@qq.com>
2023-08-09 17:32:38 +0800

    init +

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,78 @@
+<template>
+  <a-row id="globalHeader" :wrap="false" align="center">
+    <a-col flex="auto">
+      <a-menu
+        :selected-keys="selectedKeys"
+        mode="horizontal"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" />
+            <div class="title">V-Judge</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        {{ store.state.user?.loginUser?.userName ?? "未登录" }}
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script lang="ts" setup>
+import { routes } from "../router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const router = useRouter();
+//展示在菜单的路由数组
+const visibleRoutes = routes.filter((item, index) => {
+  if (item.meta?.hideInMenu) {
+    return false;
+  }
+  return true;
+});
+
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+// 路由跳转后，更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+const store = useStore();
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
